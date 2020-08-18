ef75cf32b50bd9e65f8880a274a0cdd3e9dcd4d2
Umesh Baria <ubaria@workforcelogiq.com>
2020-08-18 20:57:34 +0530

    added new table

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 mobileApp.tables.add('User'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Complaints');
 mobileApp.tables.add('FoodAccess');
+mobileApp.tables.add('Covid19Symptoms');
+mobileApp.tables.add('Covid19SymptomPeopleDetails');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
